a62c2c3f94e9891a8f8a8361d0feb699cc94dcb8
Yasuyuki Ageishi <y4suyuki@protonmail.com>
2024-12-19 19:43:05 +0900

    add pytest

--- flakes/basic/flake.nix
+++ flakes/basic/flake.nix
@@ -16,6 +16,7 @@
             pkgs.python311
             pkgs.python311Packages.pip
             pkgs.python311Packages.virtualenv
+            pkgs.python311Packages.pytest
           ];
         };
       });
